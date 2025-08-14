5c04c7ed1e41c177cb5eeae684c4601a35cb0082
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2025-08-14 12:08:29 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 200,
