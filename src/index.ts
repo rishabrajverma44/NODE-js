fdb4c672a3773475ba5757062ef722a2a303868b
rishab raj verma <rishabrajverma44@gmail.com>
2025-08-29 16:24:17 +0530

    feat: final commit

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: ["*", "http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 200,
